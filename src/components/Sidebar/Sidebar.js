08ebf683804f2ed393e4cb1d1e97aab887fdf693
Thomas Augot <thomas.augot@hotmail.fr>
2023-06-06 18:38:15 +0200

    solving sidebar swipe on mobile devices

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,16 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./Sidebar.scss";
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [currentX, setCurrentX] = useState(0);
+  const flapRef = useRef(null);
 
-  const toggleSidebar = () => {
-    setIsOpen(!isOpen);
+  const handleTouchStart = (event) => {
+    setStartX(event.touches[0].clientX);
+    setCurrentX(event.touches[0].clientX);
+  };
+
+  const handleTouchMove = (event) => {
+    setCurrentX(event.touches[0].clientX);
+  };
+
+  const handleTouchEnd = () => {
+    const differenceX = currentX - startX;
+
+    if (differenceX > 50) {
+      setIsOpen(true);
+    } else if (differenceX < -50) {
+      setIsOpen(false);
+    }
   };
 
   return (
     <div className={`sidebar ${isOpen ? "open" : ""}`}>
-      <div className="flap" onClick={toggleSidebar}></div>
+      <div
+        className="flap"
+        onClick={() => setIsOpen(!isOpen)}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+        ref={flapRef}
+      ></div>
     </div>
   );
 };
