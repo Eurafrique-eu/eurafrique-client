ca39782e3fb8c9a19133c09734b1bf624631b764
Thomas Augot <thomas.augot@hotmail.fr>
2023-06-12 20:06:30 +0200

    Benevolea translation done

--- src/pages/BENEVOLEA/BENEVOLEA.js
+++ src/pages/BENEVOLEA/BENEVOLEA.js
@@ -17,39 +17,34 @@ const BENEVOLEA = () => {
     <div className="benevolea-page">
       <div className="page-content">
         <div className="column-one">
-          <h1>DONNER DE SON TEMPS N’EST PAS PERDRE SON TEMPS</h1>
+          <h1>{t("DONNER DE SON TEMPS N’EST PAS PERDRE SON TEMPS")}</h1>
           <p>
-            Participer au projet EURAFRIQUE c’est : Choisir d’apporter sa contribution à une commu-
-            nauté et de s’impliquer dans un trajet collectif. Pouvoir grâce à des formats souples
-            d’engagement adaptables à vos disponibilités en télé-bénévolat.Un bénévolat appuyé par
-            la digitalisation pour des programmes de formation, insertion professionnelle pour les
-            populations et de soutien aux porteurs de projets de développement local.
+            {t(
+              "Participer au projet EURAFRIQUE c’est : Choisir d’apporter sa contribution à une communauté et de s’impliquer dans un trajet collectif. Pouvoir grâce à des formats souples d’engagement adaptables à vos disponibilités en télé-bénévolat.Un bénévolat appuyé par la digitalisation pour des programmes de formation, insertion professionnelle pour les populations et de soutien aux porteurs de projets de développement local."
+            )}
           </p>
-          <h1>REJOINDRE LA GRANDE CHAINE DE LA SOLIDARITE</h1>
+          <h1>{t("REJOINDRE LA GRANDE CHAINE DE LA SOLIDARITE")}</h1>
           <p>
-            Contribuer au developpement economique et social en Afrique en favorisant les activites
-            generatrices de revenus, acompagnier le developpement, l’education et la formation dans
-            les multiples defis du XXI siecle. Metre en relief la necessite d’actions citoyennes et
-            solidaires avec la mis en place une grande chaine de benevolat pour accompagner
-            concretement des projets de developemetn entreprenarial et de competences sur le
-            continent africain.{" "}
+            {t(
+              "Contribuer au développement économique et social en Afrique en favorisant les activités génératrices de revenus, accompagner le développement, l'éducation et la formation dans les multiples défis du XXIe siècle. Mettre en relief la nécessité d'actions citoyennes et solidaires avec la mise en place d'une grande chaîne de bénévolat pour accompagner concrètement des projets de développement entrepreneurial et de compétences sur le continent africain."
+            )}
           </p>
-          <h1>PARLER DE NOUS - SOUTENIR LES PROJETS DE LA COMMUNAUTE EURAFRIQUE</h1>
+          <h1>{t("PARLER DE NOUS - SOUTENIR LES PROJETS DE LA COMMUNAUTE EURAFRIQUE")}</h1>
           <h3>
             <Link to="/" className="links">
-              NOUS REJOINDRE
+              {t("NOUS REJOINDRE")}
             </Link>
           </h3>
           <p>
-            Notre force c’est votre experience et votre disponibilite Soutenir et developper les
-            capacites de chacun de nos benevole en s’appuyant sur des approches innovantes. Nous
-            aider a collecter dans financements et partenariats{" "}
+            {t(
+              "Notre force c’est votre experience et votre disponibilite Soutenir et developper les capacites de chacun de nos benevole en s’appuyant sur des approches innovantes. Nous aider a collecter dans financements et partenariats"
+            )}
           </p>
         </div>
         <div className="column-two">
           <h3>
             <Link to="/" className="links">
-              DEVENIR UN MAILLON EURAFRIQUE
+              {t("DEVENIR UN MAILLON EURAFRIQUE")}
             </Link>
           </h3>
           <img
@@ -58,31 +53,41 @@ const BENEVOLEA = () => {
             style={{ width: "150px", marginLeft: "-120px" }}
           />
           <p>
-            - MEMBRE <br /> - MEMBRE ACTIF <br />- MEMBRE BIENFAITEUR <br />- MEMBRE REFERENT <br />{" "}
-            - MEMBRE AMBASSADEUR <br />- MEMBRE VOLONTAIRE <br />
-            - MEMBRE PARTENAIRE <br />
-            - MEMBRE INSTITUTIONNEL <br />
+            {t("- MEMBRE")}
+            <br />
+            {t("- MEMBRE ACTIF")} <br />
+            {t("- MEMBRE BIENFAITEUR")} <br />
+            {t("- MEMBRE REFERENT")} <br />
+            {t("- MEMBRE AMBASSADEUR")} <br />
+            {t("- MEMBRE VOLONTAIRE")} <br />
+            {t("- MEMBRE PARTENAIRE")} <br />
+            {t("- MEMBRE INSTITUTIONNEL")} <br />
           </p>
           <br />
           <h3>
             <Link to="/" className="links">
-              RECEVOIR NOTRE LETTRE D’INFORMATION
+              {t("RECEVOIR NOTRE LETTRE D’INFORMATION")}
             </Link>
           </h3>
-          <h1>AUCUN ESPACE CIVIQUE SANS ESPACE NUMERIQUE</h1>
+          <h1>{t("AUCUN ESPACE CIVIQUE SANS ESPACE NUMERIQUE")}</h1>
           <p>
-            Réunir un maximum de talents utiles (Gestion administrative, accompagnement technique,
-            finance, marketing, communication) <br /> <br /> Echanger sur vos profils et les
-            meilleurs moyens d’agir ensemble selon vos experiences et nos idees pour “cooperer pour
-            construire l’avenir” <br />
+            {t(
+              "Réunir un maximum de talents utiles (Gestion administrative, accompagnement technique, finance, marketing, communication)"
+            )}
+            <br /> <br />
+            {t(
+              "Echanger sur vos profils et les meilleurs moyens d’agir ensemble selon vos experiences et nos idees pour “cooperer pour construire l’avenir”"
+            )}{" "}
             <br />
-            Valorisez vos centres d’interets : <br />
-            Agriculture <br />
-            Agroalimentaire <br />
-            Machinisme agricole <br />
-            Biodiversite - Nature <br />
-            Education - Formation <br /> Eau - Gestion des dechets - Pollution <br />
-            Energies{" "}
+            <br />
+            {t("Valorisez vos centres d’interets :")} <br />
+            {t("Agriculture")} <br />
+            {t("Agroalimentaire")} <br />
+            {t("Machinisme agricole")} <br />
+            {t("Biodiversite - Nature")} <br />
+            {t("Education - Formation")} <br />
+            {t("Eau - Gestion des déchets - Pollution")} <br />
+            {t("Énergies")}
           </p>
           <img
             src={require("../../../src/assets/benevolea-illu(2).PNG")}
