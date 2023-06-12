9a7ebe7a1f9cbb0af11f1622b23216c5f7f214fc
Thomas Augot <thomas.augot@hotmail.fr>
2023-06-12 16:28:50 +0200

    clicked tabs astyling updated with state logic

--- src/components/Sidebar/SidebarTabs/SidebarTabs.js
+++ src/components/Sidebar/SidebarTabs/SidebarTabs.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { useTabsContext } from "./TabsContext";
 import "./SidebarTabs.scss";
 
 function SidebarTabs() {
   const { tabs } = useTabsContext();
+  const [activeTab, setActiveTab] = useState(null);
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
 
   return (
     <div className="sidebar-tabs">
       {tabs.map((tab) => (
-        <NavLink key={tab} to={`/${tab}`} className="sidebar-tab" activeClassName="active">
+        <NavLink
+          key={tab}
+          to={`/${tab}`}
+          className={`sidebar-tab ${activeTab === tab ? "active" : ""}`}
+          onClick={() => handleTabClick(tab)}
+        >
           {tab}
         </NavLink>
       ))}
