ef61f06a68432488c71aa4324d4b002bf87dfa26
Thomas Augot <thomas.augot@hotmail.fr>
2023-06-14 18:20:37 +0200

    test

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -6,8 +6,8 @@ import MemberButton from "../memberButton/memberButton";
 import SidebarTabs from "./SidebarTabs/SidebarTabs";
 import LinksBlock from "./LinksBlock/LinksBlock";
 import { useMediaQuery } from "@mui/material";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import "./Sidebar.scss";
 
 const styles = {
@@ -22,50 +22,41 @@ const styles = {
     backgroundColor: "#f1eaeb",
   },
   container: {
-    position: "relative",
-  },
-  flap: {
-    position: "absolute",
-    top: "20%",
-    left: 0,
-    transform: "translate(-50%, -50%)",
-    width: 55,
-    height: 75,
-    background: "linear-gradient(to left, blue 0%, blue 50%, transparent 50%, transparent 100%)",
-    borderRadius: 60,
-    zIndex: 3,
-    transition: "transform 0.3s ease-in-out",
-  },
-  sidebar: {
     position: "fixed",
     top: 0,
     left: 0,
     height: "100%",
-    width: "80%",
-    maxWidth: 400,
-    backgroundColor: "#f1eaeb",
-    transition: "transform 0.3s ease-in-out",
-    zIndex: 2,
+    overflow: "hidden",
+    transition: "width 0.3s ease-in-out",
+    zIndex: 1000,
   },
   sidebarOpen: {
-    transform: "translateX(0)",
+    width: "80%",
+    // maxWidth: 400,
   },
   sidebarClosed: {
-    transform: "translateX(-100%)",
+    width: 40,
   },
-  arrowIcon: {
+  flap: {
     position: "absolute",
-    top: "50%",
-    transform: "translateY(-50%)",
-    fontSize: 40,
-    color: "#fff",
-    cursor: "pointer",
+    top: "45%",
+    left: 0,
+    transform: "translate(-50%, -50%)",
+    width: 50,
+    height: 60,
+    background: "#fff",
+    borderRadius: "50%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    boxShadow: "0 2px 9px rgb(23, 23, 136)",
+    zIndex: 3,
+    transition: "left 0.3s ease-in-out",
   },
-  leftArrowIcon: {
-    left: -20,
-  },
-  rightArrowIcon: {
-    right: -20,
+  flapIcon: {
+    fontSize: 35,
+    color: "rgb(23, 23, 136)",
+    marginLeft: "13px",
   },
 };
 
@@ -73,40 +64,58 @@ const Sidebar = ({ classes }) => {
   const [isOpen, setIsOpen] = useState(false);
   const isSmallScreen = useMediaQuery("(max-width: 768px)");
 
-  const toggleDrawer = () => {
-    setIsOpen(!isOpen);
+  const toggleDrawer = (open) => () => {
+    setIsOpen(open);
+  };
+
+  const handleTouchStart = (event) => {
+    event.stopPropagation();
+  };
+
+  const handleTouchMove = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
   };
 
   if (isSmallScreen) {
     return (
-      <div className={classes.container}>
+      <div
+        className={`${classes.container} ${isOpen ? classes.sidebarOpen : classes.sidebarClosed}`}
+      >
         <div
-          className={classes.flap}
+          className={`${classes.flap} ${isOpen ? "open" : ""}`}
           style={{
-            transform: `translate(-50%, -50%) ${isOpen ? "scale(0)" : "scale(1)"}`,
+            left: 0,
           }}
-          onClick={toggleDrawer}
+          onClick={toggleDrawer(!isOpen)}
           role="button"
-        />
+        >
+          {isOpen ? (
+            <KeyboardArrowLeftIcon className={classes.flapIcon} />
+          ) : (
+            <KeyboardArrowRightIcon className={classes.flapIcon} />
+          )}
+        </div>
         <SwipeableDrawer
+          allowSwipeInChildren={true}
           anchor="left"
           open={isOpen}
-          onClose={toggleDrawer}
-          onOpen={toggleDrawer}
+          onTouchStart={handleTouchStart}
+          onTouchMove={handleTouchMove}
+          onClose={() => setIsOpen(false)}
+          onOpen={() => setIsOpen(true)}
+          ModalProps={{
+            disableBackdropClick: false,
+            disableEscapeKeyDown: false,
+          }}
           classes={{
-            paper: `${classes.sidebar} ${isOpen ? classes.sidebarOpen : classes.sidebarClosed}`,
+            paper: classes.drawerPaper,
           }}
         >
-          <div className={`${classes.arrowIcon} ${classes.leftArrowIcon}`} onClick={toggleDrawer}>
-            <ChevronLeftIcon />
-          </div>
           <LanguagesMenu />
           <MemberButton />
           <SidebarTabs />
           <LinksBlock />
-          <div className={`${classes.arrowIcon} ${classes.rightArrowIcon}`} onClick={toggleDrawer}>
-            <ChevronRightIcon />
-          </div>
         </SwipeableDrawer>
       </div>
     );
