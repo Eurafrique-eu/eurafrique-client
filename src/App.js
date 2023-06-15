4eb3e7524eab47e0283745ffe08f8912669cc8e9
Thomas Augot <thomas.augot@hotmail.fr>
2023-06-15 19:30:49 +0200

    test

--- src/App.js
+++ src/App.js
@@ -27,60 +27,29 @@ function App() {
   const [isI18nInitialized, setIsI18nInitialized] = useState(false);
 
   useEffect(() => {
-    const initializeI18n = () => {
-      const rootElement = document.documentElement;
-      rootElement.setAttribute("translate", "no");
+    const rootElement = document.documentElement;
+    rootElement.setAttribute("translate", "no");
 
-      i18n
-        .use(LanguageDetector)
-        .use(initReactI18next)
-        .init({
-          resources: {
-            en: { translation: enTranslation },
-            fr: { translation: frTranslation },
-            es: { translation: esTranslation },
-            ar: { translation: arTranslation },
-          },
-          fallbackLng: "fr", // Default language if the user's language is not available
-          detection: {
-            order: ["localStorage", "navigator"],
-            caches: ["localStorage"],
-          },
-        })
-        .then(() => {
-          setIsI18nInitialized(true);
-        });
-    };
-
-    const handleTouchMove = (event) => {
-      const touch = event.touches[0];
-      const startX = touch.clientX;
-      const startY = touch.clientY;
-
-      const handleMove = (event) => {
-        const touch = event.touches[0];
-        const deltaX = touch.clientX - startX;
-        const deltaY = touch.clientY - startY;
-
-        // Check if the swipe is primarily horizontal (left to right)
-        if (Math.abs(deltaX) > Math.abs(deltaY)) {
-          // Prevent default behavior for horizontal swipes
-          event.preventDefault();
-        }
-      };
-
-      document.addEventListener("touchmove", handleMove, { passive: false });
-    };
-
-    initializeI18n();
-    document.addEventListener("touchstart", handleTouchMove, {
-      passive: false,
-    });
-
-    return () => {
-      document.removeEventListener("touchstart", handleTouchMove);
-    };
-  }, [isI18nInitialized]);
+    i18n
+      .use(LanguageDetector)
+      .use(initReactI18next)
+      .init({
+        resources: {
+          en: { translation: enTranslation },
+          fr: { translation: frTranslation },
+          es: { translation: esTranslation },
+          ar: { translation: arTranslation },
+        },
+        fallbackLng: "fr", // Default language if the user's language is not available
+        detection: {
+          order: ["localStorage", "navigator"],
+          caches: ["localStorage"],
+        },
+      })
+      .then(() => {
+        setIsI18nInitialized(true);
+      });
+  }, []);
 
   if (!isI18nInitialized) {
     return (
@@ -97,16 +66,85 @@ function App() {
         <div className="main-content">
           <Sidebar />
           <Routes>
-            <Route exact path="/" element={<Focus />} />
-            <Route exact path="/FOCUS" element={<Focus />} />
-            <Route exact path="/AJVI-COJEA" element={<AJVICOJEA />} />
-            <Route exact path="/POLMAR" element={<POLMAR />} />
-            <Route exact path="/AGROPOLEA" element={<AGROPOLEA />} />
-            <Route exact path="/IDEA" element={<IDEA />} />
-            <Route exact path="/BENEVOLEA" element={<BENEVOLEA />} />
-            <Route exact path="/DIGEA" element={<DIGEA />} />
-            <Route exact path="/GM-TV" element={<GMTV />} />
-            <Route path="*" element={<NotFoundPage />} />
+            <Route
+              exact
+              path="/"
+              element={<Focus />}
+              caseSensitive
+              end
+              options={{ gestureEnabled: false }}
+            />
+            <Route
+              exact
+              path="/FOCUS"
+              element={<Focus />}
+              caseSensitive
+              end
+              options={{ gestureEnabled: false }}
+            />
+            <Route
+              exact
+              path="/AJVI-COJEA"
+              element={<AJVICOJEA />}
+              caseSensitive
+              end
+              options={{ gestureEnabled: false }}
+            />
+            <Route
+              exact
+              path="/POLMAR"
+              element={<POLMAR />}
+              caseSensitive
+              end
+              options={{ gestureEnabled: false }}
+            />
+            <Route
+              exact
+              path="/AGROPOLEA"
+              element={<AGROPOLEA />}
+              caseSensitive
+              end
+              options={{ gestureEnabled: false }}
+            />
+            <Route
+              exact
+              path="/IDEA"
+              element={<IDEA />}
+              caseSensitive
+              end
+              options={{ gestureEnabled: false }}
+            />
+            <Route
+              exact
+              path="/BENEVOLEA"
+              element={<BENEVOLEA />}
+              caseSensitive
+              end
+              options={{ gestureEnabled: false }}
+            />
+            <Route
+              exact
+              path="/DIGEA"
+              element={<DIGEA />}
+              caseSensitive
+              end
+              options={{ gestureEnabled: false }}
+            />
+            <Route
+              exact
+              path="/GM-TV"
+              element={<GMTV />}
+              caseSensitive
+              end
+              options={{ gestureEnabled: false }}
+            />
+            <Route
+              path="*"
+              element={<NotFoundPage />}
+              caseSensitive
+              end
+              options={{ gestureEnabled: false }}
+            />
           </Routes>
         </div>
         <Footer />
